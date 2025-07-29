2c68617016e88aadd9d7c937f9c95f25407f6ef5
Jeff Miiller <jeffrey.miiller@gmail.com>
2025-07-29 01:27:50 -0600

    fix terraform (#25)

--- apps/portfolio-web/terraform/locals.tf
+++ apps/portfolio-web/terraform/locals.tf
@@ -1,4 +1,4 @@
 locals {
   microservice_short_name = "powa"
-  microservice_prefix = "${data.aws_caller_identity.current.account_id}-${terraform.workspace}-${microservice_short_name}"
+  microservice_prefix = "${data.aws_caller_identity.current.account_id}-${terraform.workspace}-${local.microservice_short_name}"
 }
